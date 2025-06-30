bf0905953e7a9367fd281cbf0d0a8159f27ad3a6
imteaz1213 <mdimteaz567@gmail.com>
2025-07-01 01:15:50 +0600

    first api

---
+++ Webapi/Webapi/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudentName()
+        {
+            return "John Doe";
+        }
+    }
+}
